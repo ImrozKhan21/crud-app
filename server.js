73a6060152b0d6ca27af9c331659fb9465e87abe
Imroz <imrozkhn7@gmail.com>
2019-06-02 13:10:26 -0400

    Running on node server

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
-const express = require('express');
-const path = require('path');
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
 
-const app = express();
+app.use(express.static(__dirname + "/public"));
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.get('/:date',function(req,res){
+var input = decodeURI(req.params.date);
+var isNumber = /^\d+$/.test(input);
+var unix, string;
+if(isNumber){
+  var t = new Date(parseInt(input));
+  string = t.toUTCString();
+  unix = input;
+} else {
+  unix = Date.parse(input) || null;
+  string= Date.parse(input) ? input : null;
+}
 
-app.get('/*', function(req,res) {
+var output = {
+  string: string,
+  unix:unix,
+};
 
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.json(output)
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(PORT,function(){
+  console.log('server successfully started on port '+PORT);
+});
